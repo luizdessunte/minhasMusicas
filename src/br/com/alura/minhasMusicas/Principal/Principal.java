56c9e0138311549582557b7140c43f4d6bb5fc48
luizdessunte <luizdessunte@gmail.com>
2025-05-29 11:52:30 -0300

    Initial commit

---
+++ src/br/com/alura/minhasMusicas/Principal/Principal.java
@@ -0,0 +1,4 @@
+package br.com.alura.minhasMusicas.modelos.Principal;
+
+public class Principal {
+}
---
+++ src/br/com/alura/minhasMusicas/modelos/Podcast.java
@@ -0,0 +1,22 @@
+package br.com.alura.minhasMusicas.modelos;
+
+public class Podcast extends Audio {
+    private String apresentador;
+    private String descrição;
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+}
