4bbdce30236434f10d8fa36c12a20501990e543c
Luiz Dessunte <102454818+luizdessunte@users.noreply.github.com>
2026-04-09 12:24:23 -0300

    adicionado elementos da classe Principal.java

--- src/br/com/alura/minhasMusicas/Principal/Principal.java
+++ src/br/com/alura/minhasMusicas/Principal/Principal.java
@@ -1,4 +1,35 @@
-package br.com.alura.minhasMusicas.modelos.Principal;
+package br.com.alura.minhasMusicas.Principal;
+
+import br.com.alura.minhasMusicas.modelos.Musica;
+import br.com.alura.minhasMusicas.modelos.Podcast;
 
 public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("ONE");
+        minhaMusica.setArtista("Metallica");
+
+        for (int i = 0; i< 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i< 1000; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("IA");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i< 2000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i< 1000; i++) {
+            meuPodcast.curte();
+        }
+
+
+
+    }
 }
